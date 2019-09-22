8f2a48d1f15a61db060d8d970f49cc1316b848be
Daniel Tonon <daniel.tonon@oakton.com.au>
2019-09-22 22:27:27 +1000

    Getting rid of Jest
    It's not working and all I really need is to just tell if the output has changed or not. I can do that just by keeping the output files checked into git.

--- tests/mixin/mixin.test.js
+++
@@ -1,9 +0,0 @@
-import { get_expected_output, readFile } from "../helpers"
-
-test('mixin', () => {
-	return Promise.all([readFile('./tests/mixin/mixin.test.css'), get_expected_output()])
-	.then((results => {
-		const [ compiledCSS, expectedOutput ] = results
-		expect(compiledCSS).toBe(expectedOutput)
-	}))
-})
